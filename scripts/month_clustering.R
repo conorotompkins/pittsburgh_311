c3bdc03fe76751f07fe876de2d2094cb49b7bfa3
Conor Tompkins <conorotompkins@gmail.com>
2018-02-04 19:06:23 +0000

    updated data munging script for months clustering

--- scripts/month_clustering.R
+++ scripts/month_clustering.R
@@ -13,8 +13,19 @@ df %>%
          month = month(date, label = TRUE)) -> df
 
 df %>% 
-  count(request_type, sort = TRUE)
+  filter(str_detect(request_type, "Snow/Ice")) %>%  
+  count(request_type)
 
+df %>% 
+  mutate(request_type = str_replace(request_type, "Snow/Ice removal", "Snow/Ice Removal")) -> df
+
+df %>% 
+  filter(str_detect(request_type, "Snow/Ice")) %>%  
+  count(request_type)
+
+df %>% 
+  count(request_type, sort = TRUE) %>% 
+  filter(n > 200) -> df_top_requests
 
 #df %>% 
 #  count(date, request_origin) %>% 
@@ -22,6 +33,7 @@ df %>%
 #  geom_smooth()
 
 df %>%
+  right_join(df_top_requests) %>% 
   group_by(request_type, month) %>% 
   summarize(n = n()) %>% 
   complete(request_type, month) %>% 
@@ -35,12 +47,16 @@ df_months %>%
   mutate(month_percentage = n / request_type_total) -> df_months
 
 df_months %>% 
-  ggplot(aes(Jan, Jul, group = request_type)) +
-  geom_point()
+  filter(is.na(month_percentage))
+
+df_months %>% 
+  filter(is.nan(month_percentage))
 
 df_months %>% 
   select(request_type, month, month_percentage) %>% 
   spread(month, month_percentage) -> df_months
 
-
+df_months %>% 
+  ggplot(aes(Jan, Jul)) +
+  geom_point()
 
