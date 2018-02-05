f0790ecd7985c176cc40646acb9a3c64bd4f706e
Conor Tompkins <conorotompkins@gmail.com>
2018-02-05 03:43:20 +0000

    added neighborhood PCA scripts, created plot

---
+++ scripts/neighborhood_clustering.R
@@ -0,0 +1,36 @@
+library(tidyverse)
+library(lubridate)
+
+df_raw <- read_csv("data/pittsburgh_311.csv")
+
+df <- df_raw
+
+colnames(df) <- tolower(colnames(df))
+
+df %>% 
+  mutate(request_type = str_replace(request_type, "Snow/Ice removal", "Snow/Ice Removal")) -> df
+
+df %>% 
+  count(request_type, sort = TRUE) %>% 
+  top_n(200) -> df_top_requests
+
+request_list <- unique(df_top_requests$request_type)
+
+df %>%
+  right_join(df_top_requests) %>% 
+  group_by(neighborhood, request_type) %>% 
+  summarize(n = n()) %>% 
+  complete(neighborhood, request_type = request_list) %>% 
+  replace_na(replace = list(n = 0)) -> df_nbh
+
+df_nbh %>% 
+  group_by(neighborhood) %>% 
+  mutate(neighborhood_total = sum(n)) -> df_nbh
+
+df_nbh %>% 
+  mutate(neighborhood_percentage = n / neighborhood_total) %>% 
+  arrange(neighborhood, desc(neighborhood_total), desc(neighborhood_percentage)) -> df_nbh
+
+df_nbh %>% 
+  select(request_type, neighborhood, neighborhood_percentage) %>% 
+  spread(request_type, neighborhood_percentage) -> df_nbh
---
+++ scripts/pca_neighborhood.R
@@ -0,0 +1,37 @@
+library(broom)
+
+options(scipen = 999)
+
+source("scripts/neighborhood_clustering.R")
+
+#sources
+#https://rdrr.io/cran/broom/man/prcomp_tidiers.html
+#https://poissonisfish.wordpress.com/2017/01/23/principal-component-analysis-in-r/
+#http://rstatistics.net/principal-component-analysis/
+
+set.seed(1234)
+
+df_nbh %>% 
+  ungroup() %>% 
+  remove_rownames() %>% 
+  column_to_rownames(var = "neighborhood") -> df_nbh
+
+pc <- prcomp(df_nbh, scale = TRUE)
+
+# information about rotation
+head(tidy(pc))
+
+# information about samples (neighborhoods)
+head(tidy(pc, "samples"))
+
+# information about PCs
+head(tidy(pc, "pcs"))
+
+au <- augment(pc, data = df_nbh)
+head(au)
+
+ggplot(au, aes(.fittedPC1, .fittedPC2)) +
+  geom_point() +
+  geom_label(aes(label = .rownames)) +
+  theme_bw()
+ggsave("images/311_neighborhood_PCA.png", height = 12, width = 12)
