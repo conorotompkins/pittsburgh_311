a2fb8f24712a478bb2747175cb634a76cb0b80b4
Conor Tompkins <conorotompkins@gmail.com>
2018-02-09 00:45:30 +0000

    worked on tidy pca script

--- scripts/months_pc_tidy.R
+++ scripts/months_pc_tidy.R
@@ -3,7 +3,6 @@ library(ggfortify)
 
 options(scipen = 999)
 
-
 df_months_pca <- df_months %>% 
   nest() %>% 
   mutate(pca = map(data, ~ prcomp(.x %>% select(-request_type), 
@@ -21,3 +20,20 @@ var_exp <- df_months_pca %>%
          pc = str_replace(pc, ".fitted", ""))
 
 var_exp
+
+var_exp %>% 
+  rename(
+    `Variance Explained` = var_exp,
+    `Cumulative Variance Explained` = cum_var_exp
+  ) %>% 
+  gather(key = key, value = value, `Variance Explained`:`Cumulative Variance Explained`) %>% 
+  mutate(pc = factor(pc, levels = unique(.$pc))) %>% 
+  ggplot(aes(pc, value, group = key)) + 
+  geom_point() + 
+  geom_line() + 
+  facet_wrap(~key, scales = "free_y") +
+  theme_bw() +
+  lims(y = c(0, 1)) +
+  labs(y = "Variance",
+       title = "Variance explained by each principal component")
+?fct_reorder
