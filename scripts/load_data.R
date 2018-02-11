5fbf77805490210fdcb9d7fc1742393c336b4938
Conor Tompkins <conorotompkins@gmail.com>
2018-02-11 21:10:50 +0000

    created load_data script

---
+++ scripts/load_data.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(lubridate)
+
+df_raw <- read_csv("data/pittsburgh_311.csv")
+
+df <- df_raw
+
+colnames(df) <- tolower(colnames(df))
+
+df %>%
+  mutate(date = ymd(str_sub(created_on, 1, 10)),
+         time = hms(str_sub(created_on, 11, 18)),
+         month = month(date, label = TRUE), 
+         year = year(date),
+         yday = yday(date)) -> df
+df %>% 
+  mutate(request_type = str_replace(request_type, "Snow/Ice removal", "Snow/Ice Removal")) -> df
